5b290046cc4c6b379642c9d3ab41dc7ec60dc2ab
The_Cube_Factor <75490449+galileoleosolis@users.noreply.github.com>
2025-01-02 10:52:31 -0500

    Update Adding Example.py for updates multi operational instructions

--- Adding Example.py
+++ Adding Example.py
@@ -3,6 +3,7 @@ import op
 #we are assuming that the 2 values we want to add are in registers c, and d
 
 op.alu.set_from_reg("C", "D")
+c.register.select("A") #this gets ready to store added value to register A
 c.alu_math_op("+")
 
 
